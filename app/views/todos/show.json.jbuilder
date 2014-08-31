b77a42fcbfc03e469c992679aefd8be5efa9ced6
Brantley Beaird <bbeaird@gmail.com>
2014-08-31 12:26:57 -0700

    Initial commit with rails app and todos.

---
+++ app/views/todos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo, :id, :content, :done?, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookLogin::Application.config.secret_key_base = '37d8ca0e7c16dce4d3ef856219faeddeb81bf1a2fdb47b6a13784ca95c2901a889bf7d08065c3b46718cc6e8e0d50bee3324d47e477e42f3476b1b5710cdea73'
