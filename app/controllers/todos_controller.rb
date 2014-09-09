4408e130c5999a3fe12c025a272455c8297162a0
Brantley Beaird <bbeaird@gmail.com>
2014-09-08 20:25:12 -0700

    Cleaned things up a bit.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,10 @@
 class TodosController < ApplicationController
   # before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
   def index
-    @todos = Todo.all
-    p session[:oauth] = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], ENV['FACEBOOK_CALLBACK_URL'])
-    p @auth_url = session[:oauth].url_for_oauth_code(permissions: "user_birthday,user_location,user_relationship_details,user_actions.books,user_actions.music,user_actions.movies,read_stream")
-    # can delete read_stream permission requeset
+    session[:oauth] = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], ENV['FACEBOOK_CALLBACK_URL'])
+    @auth_url = session[:oauth].url_for_oauth_code(permissions: "user_birthday,user_location,user_relationship_details,user_actions.books,user_actions.music,user_actions.movies,read_stream")
+    # delete read_stream permission requeset
   end
 
   def callback
@@ -16,15 +13,13 @@ class TodosController < ApplicationController
     end
 
     @api = Koala::Facebook::API.new(session[:access_token])
-    # @graph_data = @api.get_object("me/statuses", "fields"=>"message")
-    p @graph_data = @api.get_object("me")
-    @user_graph_data = User.new(graph_response: @graph_data.to_json, facebook_access_token: session[:access_token])
-    @user_graph_data.save
-    # redirect_to 'todos#show_some_data'
+    @graph_data = @api.get_object("me") # nested hash with data requested from permissions, to_json converts to json and stringifies.
+    @user = User.new(graph_response: @graph_data.to_json, facebook_access_token: session[:access_token])
+    @user.save
   end
 
   def show_some_data
-    p @user = JSON.parse(User.last.graph_response)
+    @user = JSON.parse(User.last.graph_response)
     @birthday = @user["birthday"]
     @age = ((Time.now - @birthday.to_datetime).to_f/60/60/24/365.25).floor
     @location = @user["location"]["name"]
